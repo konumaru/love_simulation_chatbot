ee6d3a9506a4b34a623b4989d3f1cf57071f7fb9
konumaru <konumaru1022@gmail.com>
2023-03-26 05:51:08 +0900

    make better

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 # 環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
 @app.route("/")
-def hello_world():
-    return "hello world!"
+def test():
+    return "<h1>It Works!</h1>"
 
 
 @app.route("/callback", methods=["POST"])
