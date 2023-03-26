4d556778bb5472691505ab023e8f1726794990fc
konumaru <konumaru1022@gmail.com>
2023-03-27 00:07:29 +0900

    [update] set gcloud

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,3 +1,5 @@
+gcloud config set project gpt-love
+
 heroku config:set ACCESS_TOKEN=$ACCESS_TOKEN --app gpt-love
 heroku config:set CHANNEL_SECRET=$CHANNEL_SECRET --app gpt-love
 heroku config:set OPENAI_API_KEY=$OPENAI_API_KEY --app gpt-love
